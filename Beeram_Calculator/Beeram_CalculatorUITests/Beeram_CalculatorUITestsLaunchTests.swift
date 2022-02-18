31783dacec9b212c4a15abfa84eeba58e7b55bf3
Sanjay krishna Manavarthi <89273866+Msks1218@users.noreply.github.com>
2022-02-17 18:26:48 -0600

    calc

---
+++ Beeram_Calculator/Beeram_CalculatorUITests/Beeram_CalculatorUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Beeram_CalculatorUITestsLaunchTests.swift
+//  Beeram_CalculatorUITests
+//
+//  Created by Beeram,Sai Kaushik on 2/17/22.
+//
+
+import XCTest
+
+class Beeram_CalculatorUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
